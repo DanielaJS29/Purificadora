1f458eb4ca0ac7e83a131accc16e3e3f0aaf776c
Your Name <you@example.com>
2026-07-08 16:31:49 -0600

    e sube la estructura ddel proyecto por primera vez

---
+++ purificadora/src/main/java/com/purificadora/purificadora/PurificadoraApplication.java
@@ -0,0 +1,13 @@
+package com.purificadora.purificadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurificadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurificadoraApplication.class, args);
+	}
+
+}
---
+++ purificadora/src/test/java/com/purificadora/purificadora/PurificadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purificadora.purificadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurificadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
